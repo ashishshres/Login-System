b4b42679477885556b76f0d6eba730bc9d431784
ashishshres <ashishshr27@gmail.com>
2024-05-18 16:01:39 +0545

    add connect.php

---
+++ includes/_connect.php
@@ -0,0 +1,11 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "users";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+if (!$conn) {
+    die(mysqli_connect_error());
+}
