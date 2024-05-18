0587f52154ac16bb54659ba85b7924c7f64899ea
ashishshres <ashishshr27@gmail.com>
2024-05-18 17:34:24 +0545

    add alerts

--- signup.php
+++ signup.php
@@ -1,5 +1,6 @@
 <?php
 $showAlert = false;
+$showError = false;
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     include "./includes/_connect.php";
     $email = $_POST["email"];
@@ -12,6 +13,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if ($result) {
             $showAlert = true;
         }
+    } else {
+        $showError = true;
     }
 }
 ?>
@@ -40,8 +43,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     <?php
     if ($showAlert) {
-        echo ' <div class="transition duration-300 ease-in-out toast-signup space-y-3 fixed top-20 right-0 mx-2">
-    <div class="max-w-xs bg-white border border-gray-200 rounded-xl shadow-lg" role="alert">
+        echo ' 
+    <div class="transition duration-300 ease-in-out toast-signup fixed top-20 right-0 mx-2 max-w-xs bg-white border border-gray-200 rounded-xl shadow-lg dark:bg-neutral-800 dark:border-neutral-700"" role="alert">
         <div class="flex p-4">
             <div class="flex-shrink-0">
                 <svg class="flex-shrink-0 size-4 text-teal-500 mt-0.5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
@@ -49,13 +52,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 </svg>
             </div>
             <div class="ms-3">
-                <p class="text-sm text-gray-700">
+                <p class="text-sm text-gray-700 dark:text-neutral-400">
                     Account created successfully.
                 </p>
             </div>
         </div>
     </div>
-</div>';
+';
+    }
+    if ($showError) {
+        echo '<div class="transition duration-300 ease-in-out toast-signup fixed top-20 right-0 mx-2 max-w-xs bg-white border border-gray-200 rounded-xl shadow-lg dark:bg-neutral-800 dark:border-neutral-700" role="alert">
+        <div class="flex p-4">
+          <div class="flex-shrink-0">
+            <svg class="flex-shrink-0 size-4 text-red-500 mt-0.5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"></path>
+            </svg>
+          </div>
+          <div class="ms-3">
+            <p class="text-sm text-gray-700 dark:text-neutral-400">
+              Passwords do not match.
+            </p>
+          </div>
+        </div>
+      </div>';
     }
     ?>
 
@@ -76,7 +95,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             </div>
             <div class="flex items-start mb-5">
                 <div class="flex items-center h-5">
-                    <input id="show" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
+                    <input id="show" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" />
                 </div>
                 <label for="show" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Show password</label>
             </div>
