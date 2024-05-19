3dc4debe1e9f1eeee3cb63ccf9d5da07832c3fce
ashishshres <ashishshr27@gmail.com>
2024-05-19 20:46:57 +0545

    refactor: check for duplicate entries

--- includes/_signup.php
+++ includes/_signup.php
@@ -1,20 +1,28 @@
 <?php
 $showAlert = false;
 $showError = false;
+$exists = false;
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     include "./_connect.php";
     $email = htmlspecialchars($_POST["email"]);
     $password = htmlspecialchars($_POST["password"]);
     $confirmPassword = htmlspecialchars($_POST["confirm"]);
 
-    if ($password == $confirmPassword) {
-        $sql = "INSERT INTO users (email, password) VALUES('$email', '$password')";
-        $result = mysqli_query($conn, $sql);
-        if ($result) {
-            $showAlert = true;
-        }
+    $sql = "SELECT * FROM users WHERE email = '$email'";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0) {
+        $exists = true;
     } else {
-        $showError = true;
+        if (($password == $confirmPassword) && !$exists) {
+            $sql = "INSERT INTO users (email, password) VALUES('$email', '$password')";
+            $result = mysqli_query($conn, $sql);
+            if ($result) {
+                $showAlert = true;
+            }
+        } else {
+            $showError = true;
+        }
     }
 }
 ?>
@@ -44,7 +52,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <?php
     if ($showAlert) {
         echo ' 
-    <div class="transition duration-300 ease-in-out toast-signup fixed top-20 right-0 mx-2 max-w-xs bg-white border border-gray-200 rounded-xl shadow-lg dark:bg-neutral-800 dark:border-neutral-700"" role="alert">
+    <div class="transition duration-300 ease-in-out toast-signup fixed top-20 right-0 mx-2 max-w-xs bg-white border border-gray-200 rounded-xl shadow-lg" role="alert">
         <div class="flex p-4">
             <div class="flex-shrink-0">
                 <svg class="flex-shrink-0 size-4 text-teal-500 mt-0.5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
@@ -52,7 +60,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 </svg>
             </div>
             <div class="ms-3">
-                <p class="text-sm text-gray-700 dark:text-neutral-400">
+                <p class="text-sm text-gray-900">
                     Account created successfully.
                 </p>
             </div>
@@ -61,7 +69,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 ';
     }
     if ($showError) {
-        echo '<div class="transition duration-300 ease-in-out toast-signup fixed top-20 right-0 mx-2 max-w-xs bg-white border border-gray-200 rounded-xl shadow-lg dark:bg-neutral-800 dark:border-neutral-700" role="alert">
+        echo '<div class="transition duration-300 ease-in-out toast-signup fixed top-20 right-0 mx-2 max-w-xs bg-white border border-gray-200 rounded-xl shadow-lg " role="alert">
         <div class="flex p-4">
           <div class="flex-shrink-0">
             <svg class="flex-shrink-0 size-4 text-red-500 mt-0.5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
@@ -69,8 +77,25 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             </svg>
           </div>
           <div class="ms-3">
-            <p class="text-sm text-gray-700 dark:text-neutral-400">
+            <p class="text-sm text-gray-900 ">
               Passwords do not match.
+            </p>
+          </div>
+        </div>
+      </div>';
+    }
+
+    if ($exists) {
+        echo '<div class="transition duration-300 ease-in-out toast-signup fixed top-20 right-0 mx-2 max-w-xs bg-white border border-gray-200 rounded-xl shadow-lg " role="alert">
+        <div class="flex p-4">
+          <div class="flex-shrink-0">
+            <svg class="flex-shrink-0 size-4 text-red-500 mt-0.5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"></path>
+            </svg>
+          </div>
+          <div class="ms-3">
+            <p class="text-sm text-gray-900 ">
+              Email already exists.
             </p>
           </div>
         </div>
