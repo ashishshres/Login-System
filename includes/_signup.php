477595d2369d761aaf06c00e2f530ac0cccfe08b
ashishshres <ashishshr27@gmail.com>
2024-05-19 20:51:15 +0545

    refactor: add comment

--- includes/_signup.php
+++ includes/_signup.php
@@ -9,6 +9,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $password = htmlspecialchars($_POST["password"]);
     $confirmPassword = htmlspecialchars($_POST["confirm"]);
 
+    // check for the duplicate entries
     $sql = "SELECT * FROM users WHERE email = '$email'";
     $result = mysqli_query($conn, $sql);
     if (mysqli_num_rows($result) > 0) {
