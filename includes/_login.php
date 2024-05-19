664dc0dd02461bda4f7bb125dfe20374ae27fa0b
ashishshres <ashishshr27@gmail.com>
2024-05-19 20:47:09 +0545

    refactor: update login

--- includes/_login.php
+++ includes/_login.php
@@ -1,5 +1,4 @@
 <?php
-$login = false;
 $showError = false;
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     include "./_connect.php";
@@ -11,7 +10,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $isFound = mysqli_num_rows($result);
     if ($isFound == 1) {
-        $login = true;
         session_start();
         $_SESSION["loggedin"] = true;
         $_SESSION["email"] = $email;
@@ -45,39 +43,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </nav>
 
     <?php
-    if ($login) {
-        echo ' 
-    <div class="transition duration-300 ease-in-out toast-signin fixed top-20 right-0 mx-2 max-w-xs bg-white border border-gray-200 rounded-xl shadow-lg dark:bg-neutral-800 dark:border-neutral-700"" role="alert">
+    if ($showError) {
+        echo '<div class="transition duration-300 ease-in-out toast-signin fixed top-20 right-0 mx-2 max-w-xs bg-white border border-gray-200 rounded-xl shadow-lg " role="alert">
         <div class="flex p-4">
             <div class="flex-shrink-0">
-                <svg class="flex-shrink-0 size-4 text-teal-500 mt-0.5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"></path>
+                <svg class="flex-shrink-0 size-4 text-red-500 mt-0.5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"></path>
                 </svg>
             </div>
             <div class="ms-3">
-                <p class="text-sm text-gray-700 dark:text-neutral-400">
-                    You are logged in.
-                </p>
+            <p class="text-sm text-gray-900">
+                Invalid credentials.
+            </p>
             </div>
         </div>
-    </div>
-';
-    }
-    if ($showError) {
-        echo '<div class="transition duration-300 ease-in-out toast-signin fixed top-20 right-0 mx-2 max-w-xs bg-white border border-gray-200 rounded-xl shadow-lg dark:bg-neutral-800 dark:border-neutral-700" role="alert">
-        <div class="flex p-4">
-          <div class="flex-shrink-0">
-            <svg class="flex-shrink-0 size-4 text-red-500 mt-0.5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"></path>
-            </svg>
-          </div>
-          <div class="ms-3">
-            <p class="text-sm text-gray-700 dark:text-neutral-400">
-              Invalid credentials.
-            </p>
-          </div>
-        </div>
-      </div>';
+    </div>';
     }
     ?>
 
