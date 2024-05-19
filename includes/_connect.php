1415d88b0877cf2baef8b7944ace7a2a2305cfb7
ashishshres <ashishshr27@gmail.com>
2024-05-19 20:51:07 +0545

    refactor: add comment

--- includes/_connect.php
+++ includes/_connect.php
@@ -1,11 +1,15 @@
 <?php
+
+// server variables
 $servername = "localhost";
 $username = "root";
 $password = "";
 $database = "users";
 
+// creating connection
 $conn = mysqli_connect($servername, $username, $password, $database);
 
+// testing the connection
 if (!$conn) {
     die(mysqli_connect_error());
 }
